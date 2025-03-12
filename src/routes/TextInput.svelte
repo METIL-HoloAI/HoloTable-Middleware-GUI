ee368cc0ddc034697b10aff841413f67a08fbfb8
Pharit Smitasin <pharitsmitasin@gmail.com>
2025-03-11 21:51:12 -0400

    replace input component w/shad Textarea

--- src/routes/TextInput.svelte
+++ src/routes/TextInput.svelte
@@ -3,6 +3,7 @@
 	import { Button } from '$lib/components/ui/button/index.js';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { SendHorizontalIcon } from '@lucide/svelte';
+	import { Textarea } from '$lib/components/ui/textarea/index.js';
 
 	const gs = new GlobalState();
 
@@ -12,26 +13,29 @@
 	const onclick = () => gs.reset();
 </script>
 
-<Card.Content class="p-6">
-	<form {onsubmit} class="space-y-3">
-		<div class="flex w-full max-w-sm items-center space-x-2">
-			<Input
-				type="text"
-				placeholder="Enter your prompt"
-				bind:value={gs.name}
-				class="w-full px-4 py-2 rounded-lg border focus:ring-2 focus:ring-black focus-visible:ring-black transition-all duration-200"
-			/>
+<div>
+	<Card.Content class="p-6">
+		<form {onsubmit} class="space-y-3">
+			<div class="flex w-full max-w-sm items-center space-x-2">
+				<!-- <Input
+					type="text"
+					placeholder="Enter your prompt"
+					bind:value={gs.name}
+					class="w-full px-4 py-2 rounded-lg border focus:ring-2 focus:ring-black focus-visible:ring-black hover:opacity-90 transition-all duration-200"
+				/> -->
+				<Textarea placeholder="Type your prompt for the HoloTable here." id="message-2" />
+				<Button
+					type="submit"
+					class="w-min rounded-full text-black bg-gradient-to-r from-white to-white/95 transition-opacity duration-200 hover:opacity-80"
+				>
+					<SendHorizontalIcon />
+				</Button>
+			</div>
 			<Button
-				type="submit"
-				class="w-min rounded-full text-black bg-gradient-to-r from-white to-white/95 transition-opacity duration-200"
+				{onclick}
+				class="w-full bg-gradient-to-r from-slate-300/50 to-slate-400/50 hover:ring-1 hover:opacity-80 transition-all duration-200"
+				>Reset</Button
 			>
-				<SendHorizontalIcon />
-			</Button>
-		</div>
-		<Button
-			{onclick}
-			class="w-full bg-gradient-to-r from-black to-black/50 hover:ring-1 hover:ring-slate-200 transition-all duration-200"
-			>Reset</Button
-		>
-	</form>
-</Card.Content>
+		</form>
+	</Card.Content>
+</div>
