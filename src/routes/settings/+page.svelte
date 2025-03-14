1c0874b8a109f90fc2b4f030a490063316447742
Pharit Smitasin <pharitsmitasin@gmail.com>
2025-03-14 19:14:10 -0400

    implement settings page GET methods working for general config files

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -2,13 +2,75 @@
 	import { GlobalState, preventDefault } from '$lib';
 	import * as Card from '$lib/components/ui/card/index.js';
 	import { CornerDownLeftIcon } from '@lucide/svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Tooltip from '$lib/components/ui/tooltip';
+	import { fade } from 'svelte/transition';
+
+	import { ScrollArea } from '$lib/components/ui/scroll-area/index.js';
+	import { Separator } from '$lib/components/ui/separator/index.js';
 
 	const gs = new GlobalState();
 
-	$inspect(gs.greet, gs.name);
+	const configs = [
+		{ name: 'speechtotext', directory: '' },
+		{ name: 'general', directory: '' },
+		{ name: 'intentdetection', directory: '' },
+		{ name: '3dgen', directory: 'contentgen_yamls' },
+		{ name: 'gifgen', directory: 'contentgen_yamls' },
+		{ name: 'imagegen', directory: 'contentgen_yamls' },
+		{ name: 'videogen', directory: 'contentgen_yamls' },
+		{ name: '3dworkflow', directory: 'contentgen_workflows' },
+		{ name: 'gifworkflow', directory: 'contentgen_workflows' },
+		{ name: 'imageworkflow', directory: 'contentgen_workflows' },
+		{ name: 'videoworkflow', directory: 'contentgen_workflows' }
+	];
 
-	const onsubmit = preventDefault(() => gs.nlen && gs.submit());
-	const onclick = () => gs.reset();
+	const configClicked = (config: string, configDirectory: string) => {
+		gs.selectedConfig = config;
+		gs.selectedConfigDirectory = configDirectory;
+		console.log(
+			'selected config: ' + gs.selectedConfig + ' with directory: ' + gs.selectedConfigDirectory
+		);
+		loadYaml();
+	};
+
+	const onReset = () => {
+		gs.reset();
+		yamlContent = '';
+	};
+
+	let yamlContent = '';
+
+	async function loadYaml() {
+		if (gs.selectedConfigDirectory === '') {
+			try {
+				const response = await fetch(`http://localhost:8000/config/${gs.selectedConfig}`, {
+					method: 'GET'
+				});
+				if (!response.ok) {
+					throw new Error(`HTTP error ${response.status}`);
+				}
+				yamlContent = await response.text();
+			} catch (error) {
+				console.error('Error fetching YAML:', error);
+			}
+		} else {
+			try {
+				const response = await fetch(
+					`http://localhost:8000/config/${gs.selectedConfigDirectory}/${gs.selectedConfig}`,
+					{
+						method: 'GET'
+					}
+				);
+				if (!response.ok) {
+					throw new Error(`HTTP error ${response.status}`);
+				}
+				yamlContent = await response.text();
+			} catch (error) {
+				console.error('Error fetching YAML:', error);
+			}
+		}
+	}
 </script>
 
 <div
@@ -32,5 +94,29 @@
 				</div>
 			</Card.Title>
 		</Card.Header>
+		<ScrollArea class="h-3/4 mt-4 rounded-md border bg-slate-900/75 text-white overflow-visible">
+			{#if gs.selectedConfig === ''}
+				<div class="p-4" in:fade={{ duration: 200 }}>
+					{#each configs as config}
+						<button
+							type="button"
+							class="text-sm h-10 justify-center hover:bg-slate-500/75 hover:cursor-pointer transition-all duration-200 flex items-center w-full"
+							on:click={() => configClicked(config.name, config.directory)}
+						>
+							{config.name}
+						</button>
+						<Separator />
+					{/each}
+				</div>
+			{/if}
+
+			{#if yamlContent}
+				<pre>{yamlContent}</pre>
+			{/if}
+		</ScrollArea>
+		{#if gs.selectedConfig !== ''}
+			<Button onclick={onReset}>Cancel</Button>
+			<Button>Apply</Button>
+		{/if}
 	</Card.Root>
 </div>
