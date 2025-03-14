ccf614428489e7f407b2d1c8531a880d2c4ca62c
Pharit Smitasin <pharitsmitasin@gmail.com>
2025-03-14 19:07:19 -0400

    add globalstates for configs

--- src/lib/commands.svelte.ts
+++ src/lib/commands.svelte.ts
@@ -8,7 +8,7 @@ export const preventDefault = <T extends Event>(fn: (e: T) => void): ((e: T) => 
 };
 
 export class GlobalState {
-	private _state = $state({ prompt: '' });
+	private _state = $state({ prompt: '', selectedConfig: '', selectedConfigDirectory: '' });
 	public isVoiceInput = $state(false);
 
 	get prompt() {
@@ -20,12 +20,29 @@ export class GlobalState {
 	get nlen() {
 		return this.prompt.length;
 	}
+	get selectedConfig() {
+		return this._state.selectedConfig;
+	}
+	set selectedConfig(value: string) {
+		this._state.selectedConfig = value;
+	}
+	get selectedConfigDirectory() {
+		return this._state.selectedConfigDirectory;
+	}
+	set selectedConfigDirectory(value: string) {
+		this._state.selectedConfigDirectory = value;
+	}
 
 	// async submit() {
 	// 	this.greet = await invoke('greet', { name: this.prompt });
 	// }
+	async submit() {
+		// submission logic here
+	}
 
 	reset() {
 		this.prompt = '';
+		this.selectedConfig = '';
+		this.selectedConfigDirectory = '';
 	}
 }
