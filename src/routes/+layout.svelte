e73e83601e321b27bca843f142f397f65f04d31a
Pharit Smitasin <pharitsmitasin@gmail.com>
2025-03-31 16:40:19 -0400

    add call for websocket connection in layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import '../app.css';
 	import { Toaster } from '$lib/components/ui/sonner';
+	import { onMount } from 'svelte';
+	import { connectWebsocket } from '$lib/websocket';
 	let { children } = $props();
+	onMount(() => {
+		connectWebsocket();
+	});
 </script>
 
 <Toaster />
